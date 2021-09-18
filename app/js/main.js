71f0f37a7fc78631b97567e7ffc5499a7514a2cb
Schur33 <can4ik22@gmail.com>
2021-09-18 13:57:02 +0300

    v1.1 added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
-$ (function() {
-  
+$(function () {
+  $('.preview__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick__btn slick-prev"><img src="images/prevarrow.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick__btn slick-next"><img src="images/nextarrow.svg" alt=""></button>',
+  });
 })
